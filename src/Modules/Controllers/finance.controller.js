786e08857a0ae684dd2e8d5e168fd4ee583d327c
Yureva Lyubov <yureva.exceedteam@gmail.com>
2022-03-31 15:32:59 +0300

    Create rout update finance

--- src/Modules/Controllers/finance.controller.js
+++ src/Modules/Controllers/finance.controller.js
@@ -29,3 +29,19 @@ module.exports.deleteFinance = (req, res) => {
     res.send(result);
   });
 };
+
+module.exports.updateFinance = (req, res) => {
+  const { body } = req;
+  const { _id } = body;
+  if (!body.hasOwnProperty("nameCompany")) return res.status(404).send("Error! Company not found");
+  if (!body.hasOwnProperty("money")) return res.status(404).send("Error! Money not found");
+  if (!body.hasOwnProperty("_id")) return res.status(404).send("Error! Id not found");
+  if (typeof body.money !== "number") return res.status(422).send("Error! Money value not correct");
+  if (typeof _id !== "string") return res.status(422).send("Error! Id value not correct");
+
+  const newFinance = body;
+  Finance.findOneAndUpdate({ _id }, newFinance, {new: true}, (err, result) => {
+    if (err) return res.status(404).send('Finance not found');
+    res.send(result);
+  });
+};
--- src/Modules/Routers/finance.router.js
+++ src/Modules/Routers/finance.router.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { allFinance, createFinance, deleteFinance } = require("../Controllers/finance.controller");
+const { allFinance, createFinance, deleteFinance, updateFinance } = require("../Controllers/finance.controller");
 
 router.get("/allFinance", allFinance);
 router.post("/createFinance", createFinance);
 router.delete("/deleteFinance", deleteFinance);
+router.patch("/updateFinance", updateFinance);
 
 module.exports = router;
